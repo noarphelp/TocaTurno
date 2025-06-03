fa5fc94d29645f7c1cb3c119a424a1d97613d037
noarphelp <ordestrader@gmail.com>
2025-06-03 16:24:24 +0200

    avance en creación de turnos dia 3

--- TocaTurno/src/main/java/servlets/TurnoEstadoServlet.java
+++ TocaTurno/src/main/java/servlets/TurnoEstadoServlet.java
@@ -10,8 +10,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = "/turnoEstado")
 public class TurnoEstadoServlet extends HttpServlet {
@@ -21,22 +23,32 @@ public class TurnoEstadoServlet extends HttpServlet {
 
         TurnoController turnoController = new TurnoController();
 
-        //AQUI COGEMOS EL PARAMETRO ESTADO Y DEVOLVEMOS LA LISTA DE TURNOS ECONTRADOS POR ESTADOS IGUALES AL PARAMETRO.
+        //AQUI COGEMOS EL PARAMETRO ESTADO Y LA FECHA DESEADA POR EL USUARIO Y DEVOLVEMOS LA LISTA DE TURNOS ECONTRADOS.
         String estadoParam = req.getParameter("estado");
+        String fechaParam = req.getParameter("fecha");
+
         List<Turno> lista = new ArrayList<>();
 
         if (estadoParam != null && !estadoParam.isEmpty()) {
             try {
                 Estado estado = Estado.valueOf(estadoParam);
-                lista = turnoController.findEstado(estado);
-            } catch (IllegalArgumentException e) {
+                LocalDate fecha = LocalDate.parse(fechaParam);
 
+                List<Turno> turnos = turnoController.findEstado(estado);
+                if (turnos != null) {
+                    lista = turnos.stream()
+                            .filter(e -> e.getFecha().toLocalDate().equals(fecha))
+                            .collect(Collectors.toList());
+                }
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
         req.setAttribute("listaTurnosEstado", lista);
         req.getRequestDispatcher("turnoEstado.jsp").forward(req, resp);
-
     }
 
 }
