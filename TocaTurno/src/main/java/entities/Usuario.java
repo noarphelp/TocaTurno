ae338325d579e20e377431bcaee0c2e1fd514666
Fatine <fatinemamnoun@gmail.com>
2025-06-04 13:12:18 +0200

    usuario

--- TocaTurno/src/main/java/entities/Usuario.java
+++ TocaTurno/src/main/java/entities/Usuario.java
@@ -1,11 +1,9 @@
 package entities;
 
 import jakarta.persistence.*;
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
 
 @Entity
 public class Usuario {
